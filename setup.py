0054233bda83b82346fb50ffa74492e3e83984d6
Juliet Hougland <juliet@cloudera.com>
2015-08-28 18:15:14 -0700

    Initial outline of dirs and a simple setup.py.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ts-analysis',
+    version='0.0.1',
+    author='Sean Owen, Sandy Ryza, Juliet Hougland',
+    author_email='sowen@cloudera.com, sryza@cloudera.com, juliet@cloudera.com',
+    packages=find_packages(),
+    description='Time series analysis of stock ticker and wikipedia pages view data.',
+    long_description='Source code to accompany "Data Science for Wall Street" tutorial at Hadoop World NYC 2015',
+    install_requires=[
+        'scipy==0.16.0',
+        'numpy==1.9.2'
+    ],
+)
